4b9d513f752eaad43d0788b48856df82d0e518d8
Wade Fleming <wadefleming2000@hotmail.com>
2020-12-26 18:09:01 +0200

    added DateFormatter utility class

---
+++ SlotBooker.Services/Utils/DateFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace SlotBooker.Services.Utils
+{
+    public class DateFormatter
+    {
+        private readonly DateTime date;
+        private const string english = "en-GB"; // ensure date naming matches target website
+
+        public string MonthString => date.ToString("MMMM", CultureInfo.GetCultureInfo(english));
+        public string DateString => date.ToString("MMMM dd, yyyy", CultureInfo.GetCultureInfo(english));
+
+        public DateFormatter(DateTime date)
+        {
+            this.date = date;
+        }
+    }
+}
