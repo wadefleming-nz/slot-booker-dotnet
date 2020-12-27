4b90e46990f2626e4dade8ba382f50b7311d6496
Wade Fleming <wadefleming2000@hotmail.com>
2020-12-27 12:07:01 +0200

    set Find button as Accept button

--- SlotBooker.UI/BookSlotForm.Designer.cs
+++ SlotBooker.UI/BookSlotForm.Designer.cs
@@ -139,6 +139,7 @@ namespace SlotBooker.UI
             // 
             // BookSlotForm
             // 
+            this.AcceptButton = this.findSlotButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(521, 750);
